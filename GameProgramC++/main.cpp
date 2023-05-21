8009b1de198176061b5eb247029e1eecdd5d3f78
kuranigit <shuntaf1220@gmail.com>
2023-05-22 01:36:20 +0900

    入力毎に画面が点滅するバグを修正

--- GameProgramC++/main.cpp
+++ GameProgramC++/main.cpp
@@ -5,45 +5,48 @@
 #define height 25
 using namespace std;
 #include "object.h"
+vector<string>s(height);
+
+void DrawScreen() {
+	//system("cls");
+	cout << "\033[0;0H";
+	for (int i = 0; i < height; i++)cout << s[i] << endl;
+	
+}
 
 int main() {
 	char input;
-	vector<string>s(height);
 	int num1 = 0, num2 = 0;
-	for (int i = 0; i < height; i++)for (int j = 0; j < width; j++)s[i].push_back(' ');
-	s[0][0] = '*';
-	for (int i = 0; i < height; i++)cout << s[i] << endl;
+	for (int i = 0; i < height; i++)for (int j = 0; j < width; j++)s[i].push_back('-');
+	s[0][0] = '&';
+	DrawScreen();
 
 	while (true)
 	{
 		input = getch();
 
 		if (input == 'd' && num2 < width - 1) {
-			s[num1][num2] = ' ';
-			s[num1][num2 + 1] = '*';
-			system("cls");
-			for (int i = 0; i < height; i++)cout << s[i] << endl;
+			s[num1][num2] = '-';
+			s[num1][num2 + 1] = '&';
+			DrawScreen();
 			num2++;
 		}
 		else if (input == 'a' && num2 > 0) {
-			s[num1][num2] = ' ';
-			s[num1][num2 - 1] = '*';
-			system("cls");
-			for (int i = 0; i < height; i++)cout << s[i] << endl;
+			s[num1][num2] = '-';
+			s[num1][num2 - 1] = '&';
+			DrawScreen();
 			num2--;
 		}
 		else if (input == 'w' && num1 > 0) {
-			s[num1][num2] = ' ';
-			s[num1 - 1][num2] = '*';
-			system("cls");
-			for (int i = 0; i < height; i++)cout << s[i] << endl;
+			s[num1][num2] = '-';
+			s[num1 - 1][num2] = '&';
+			DrawScreen();
 			num1--;
 		}
 		else if (input == 's' && num1 < height - 1) {
-			s[num1][num2] = ' ';
-			s[num1 + 1][num2] = '*';
-			system("cls");
-			for (int i = 0; i < height; i++)cout << s[i] << endl;
+			s[num1][num2] = '-';
+			s[num1 + 1][num2] = '&';
+			DrawScreen();
 			num1++;
 		}
 		else if (input == '0') {
@@ -54,24 +57,3 @@ int main() {
 
 	return 0;
 }
-
-//int main(void)
-//{
-//	int num;
-//
-//	char buf[] = "ABCDDEFG";
-//	char buf2[] = "123456789";
-//
-//	//3バイトだけコピー
-//	//memcpy(buf, buf2, 3);
-//
-//	//表示
-//	printf("コピー後のbuf文字列→%s\n", buf);
-//
-//	cin >> num;
-//	if(num == 1)memcpy(buf, buf2, 3);
-//
-//	printf("コピー後のbuf文字列→%s\n", buf);
-//
-//	return 0;
-//}
